17e2631e6302132e77fbf4961947dcfb84918d6d
maihiang <oumaekumiko160@outlook.com>
2024-01-20 19:25:50 +0800

    add times

--- main.c
+++ main.c
@@ -1,6 +1,12 @@
 #include<stdio.h>
+int times(int b,int c);
 int main(void){
     printf("Hello World\n");
     system("Pause");
+
+    int a,b,c;
+    scanf("%d %d",&b,&c);
+    a=times(b,c);
+    printf("%d",a);
     return 0;
 }
---
+++ times.c
@@ -0,0 +1,5 @@
+int times(int b,int c){
+    int a;
+    a=b*c;
+    return a;
+}
