4c6e2f789821aa4a49efe3bd1dd1208c416c017b
maihiang <oumaekumiko160@outlook.com>
2024-01-20 19:17:59 +0800

    add plus and minus

--- main.c
+++ main.c
@@ -2,5 +2,20 @@
 int main(void){
     printf("Hello World\n");
     system("Pause");
+    printf("Please select one mode:\n1 for plus\n2 for minus\nPlease choose your mode:");
+    int a,b,c,d;
+    
+    scanf("%d",&a);
+
+    printf("Please input 2 numbers:");
+    scanf("%d%d",&b,&c);
+
+    switch(a){
+        case 1:d=b+c;break;
+        case 2:d=b-c;break;
+        default:d=0;break;
+    }
+    printf("%d",d);
+
     return 0;
 }
